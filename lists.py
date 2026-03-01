0e9acabfc910f07abe6c60cfd6865bddad8b5f42
UzairCoding <ambaraliriza@gmail.com>
2026-03-01 13:28:57 +0000

    lists

---
+++ lists.py
@@ -0,0 +1,13 @@
+
+empty_list = []
+print()
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+triple_numbers = [1, 2, 3] * 3
+print(triple_numbers)
+
+aList = [100, 200, 300, 400, 500]
+aList = aList[::-1]
+print(aList,"/n")
